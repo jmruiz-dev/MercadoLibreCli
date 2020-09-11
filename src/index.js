5a2f19d57773a9a30e080c2505e73f7b0862750e
JoseDev <jmruiz0894@gmail.com>
2020-09-11 00:42:21 -0300

    ✨ creating process to retrieve data from ML API

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@
 
  */
 
-import https from 'https';
+import 'babel-polyfill';
 import {program} from 'commander';
+import axios from 'axios';
 import pkg from '@package';
 
 const exits = {
@@ -22,9 +23,29 @@ const getScriptName = ({bin} = {}, name) => {
 const splitList = (value) => {
     return value.split(",");
 };
-const main = function ({seller_id, site_id}) {
 
-    console.log(JSON.stringify({seller_id, site_id}));
+const labelMaxLength = 20;
+const idMaxLength = 15;
+const formatText = function (text, maxlength = 0) {
+    const factor = text.length > maxlength ? 0 : maxlength - text.length;
+    return "'" + text + "'" + ' '.repeat(factor)
+};
+const main = async function ({seller_id: sellerIds, site_id: siteId, extended}) {
+
+    for (const currentSellerId of sellerIds) {
+        const {data} = await axios.get(`https://api.mercadolibre.com/sites/${siteId}/search?seller_id=${currentSellerId}`);
+
+        for (const row of data.results) {
+
+            const {id, title, category_id, seller: {nickname}} = row;
+
+            const {data: {name: categoryName}} = await axios.get(`https://api.mercadolibre.com/categories/${category_id}`);
+
+            console.log((extended?`Provider:${formatText(nickname, labelMaxLength)} `:'')+`Id:${formatText(id, idMaxLength)} CategoryName:${formatText(categoryName, labelMaxLength)} CategoryId:${formatText(category_id, labelMaxLength)} Title:'${title}' `)
+        }
+
+
+    }
 
 
     process.exit(exits.success);
@@ -36,5 +57,6 @@ program
     .usage("[global options]")
     .requiredOption('--seller_id <seller>', 'Please define seller_id [ You could define many sellers in a comma separated list ] ', splitList)
     .requiredOption('--site_id <site>', 'Please define site_id')
-    .action(main)
+    .option('-e, --extended', 'display provider name for each product', false)
+    .action((...args) => main(...args))
     .parse(process.argv);
